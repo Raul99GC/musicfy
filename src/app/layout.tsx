87f118b31f2b7b04eb7ab2cbf6926f338134a714
Raul99GC <raulc99g@gmail.com>
2024-08-09 19:33:06 -0600

    adds return type and adds overflow style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import './globals.css'
 
-const nunito = Nunito({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Spotify|clone",
-  description: "The best place to listen to music ",
-};
+  title: 'Spotify|clone',
+  description: 'The best place to listen to music '
+}
 
-export default function RootLayout({
-  children,
+export default function RootLayout ({
+  children
 }: Readonly<{
-  children: React.ReactNode;
-}>) {
+  children: React.ReactNode
+}>): JSX.Element {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={nunito.className}>
-        <div className="w-full h-screen max-h-screen bg-music-dark text-music-light">
+        <div className='w-full h-screen max-h-screen bg-music-dark text-music-light overflow-hidden'>
           {children}
         </div>
       </body>
     </html>
-  );
+  )
 }
