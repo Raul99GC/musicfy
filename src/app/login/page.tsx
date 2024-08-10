ca5383ff22486ffe5cb4c95c63dadb84cdced2bb
Raul99GC <raulc99g@gmail.com>
2024-08-09 21:10:54 -0600

    format code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,20 @@
-import Image from "next/image";
+import Image from 'next/image'
 
-import logoImage from '/public/spotify-logo-img.png'
-import { ButtonLogin } from "@/components/login/login-button";
-
+import logoImage from '../../../../public/spotify-logo-img.png'
+import { ButtonLogin } from '@/components/login/login-button'
 
 const Page: React.FC = () => {
   return (
     <div className='w-screen h-screen flex flex-col items-center justify-center gap-14'>
-        <Image
-          src={logoImage}
-          alt='spotify logo'
-          className='w-[90%] lg:w-1/4'
-          priority={true}
-        />
-        <ButtonLogin field="Login" />
-      </div>
-  );
-};
+      <Image
+        src={logoImage}
+        alt='spotify logo'
+        className='w-[90%] lg:w-1/4'
+        priority
+      />
+      <ButtonLogin field='Login' />
+    </div>
+  )
+}
 
-export default Page;
+export default Page
