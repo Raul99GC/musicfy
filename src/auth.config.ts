697ce88fa6d425bbe44d5572b5f43d5c9b755f24
Raul99GC <raulc99g@gmail.com>
2024-08-09 19:15:34 -0600

    add basics configs with spotify provider

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,4 +1,4 @@
-import Spotify from "next-auth/providers/spotify"
-import type { NextAuthConfig } from "next-auth"
- 
+import Spotify from 'next-auth/providers/spotify'
+import type { NextAuthConfig } from 'next-auth'
+
 export default { providers: [Spotify] } satisfies NextAuthConfig
